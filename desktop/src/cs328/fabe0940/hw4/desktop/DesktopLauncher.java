374b2b40becfaba6573bc7a5bbb45f2b340c4024
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-03-28 14:51:17 -0700

    initial commit

---
+++ desktop/src/cs328/fabe0940/hw4/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cs328.fabe0940.hw4.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cs328.fabe0940.hw4.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
