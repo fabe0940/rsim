ceecb47399d819b4689bec28a29dd70e5cd56aba
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-03-28 15:23:05 -0700

    added Main skeleton

--- core/src/cs328/fabe0940/hw4/Main.java
+++ core/src/cs328/fabe0940/hw4/Main.java
@@ -1,27 +1,77 @@
 package cs328.fabe0940.hw4;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Main implements ApplicationListener, InputProcessor {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Gdx.app.setLogLevel(Application.LOG_INFO);
+		/* Gdx.app.setLogLevel(Application.LOG_DEBUG); */
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void dispose() {
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public boolean keyDown (int key) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp (int key) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped (char ch) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown (int x, int y, int ptr, int btn) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp (int x, int y, int ptr, int btn) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged (int x, int y, int ptr) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved (int x, int y) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled (int val) {
+		return false;
 	}
 }
