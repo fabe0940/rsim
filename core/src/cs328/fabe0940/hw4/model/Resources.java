7265d9ed4656347f0a45d43c98fd1dba96fdf38c
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-03-29 18:53:09 -0700

    added resource constants

---
+++ core/src/cs328/fabe0940/hw4/model/Resources.java
@@ -0,0 +1,12 @@
+package cs328.fabe0940.hw4.model;
+
+public class Resources {
+	public static final int NUM = 7;
+	public static final int WOOD = 1;
+	public static final int STONE = 2;
+	public static final int BRICK = 3;
+	public static final int ORE = 4;
+	public static final int GLASS = 5;
+	public static final int PAPER = 6;
+	public static final int CLOTH = 7;
+}
