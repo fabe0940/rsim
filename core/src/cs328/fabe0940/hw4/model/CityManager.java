3978a0604341bd468e326457ccd95e8d86d75c75
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-03-29 18:53:43 -0700

    implemented CityManager class

---
+++ core/src/cs328/fabe0940/hw4/model/CityManager.java
@@ -0,0 +1,58 @@
+package cs328.fabe0940.hw4.model;
+
+import java.util.List;
+import java.util.ArrayList;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
+public final class CityManager {
+	public static final CityManager instance = new CityManager();
+
+	public List<City> cities;
+	public City current;
+
+	private CityManager() {
+		if(instance != null) {
+			throw new IllegalStateException("reinstantiating singleton");
+		}
+
+		cities = new ArrayList<City>();
+
+		loadFromFile("city/list.txt");
+	}
+
+	public void loadFromFile(String fname) {
+		int i;
+		String fcontents;
+		String[] vals;
+		FileHandle fin;
+		City c;
+
+		fin = Gdx.files.internal(fname);
+		fcontents = fin.readString();
+		vals = fcontents.trim().split("\\s+");
+
+		for(i = 0; i < vals.length; i++) {
+			c = new City();
+			c.loadFromFile(vals[i]);
+			cities.add(c);
+		}
+
+		i = (int) Math.floor(Math.random() * (cities.size() + 1));
+		current = cities.get(i);
+	}
+
+	public void update() {
+		for(City c : cities) {
+			c.update();
+		}
+	}
+
+	public void setCurrent(String name) {
+		for(City c : cities) {
+			if(c.name == name) {
+				current = c;
+			}
+		}
+	}
+}
